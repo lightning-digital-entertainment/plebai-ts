6ba6bbeec08e6d4e896986d3efa5e2d45eb9d426
Egge <egge24@protonmail.com>
2023-10-18 13:37:23 +0200

    added build process

---
+++ build.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const esbuild = require('esbuild')
+
+let common = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  sourcemap: 'external',
+}
+
+
+esbuild
+  .build({
+    ...common,
+    outdir: 'lib/esm',
+    format: 'esm',
+    packages: 'external',
+  })
+  .then(() => {
+    const packageJson = JSON.stringify({ type: 'module' })
+    fs.writeFileSync(`${__dirname}/lib/esm/package.json`, packageJson, 'utf8')
+
+    console.log('esm build success.')
+  })
+
+esbuild
+  .build({
+    ...common,
+    outdir: 'lib/cjs',
+    format: 'cjs',
+    packages: 'external',
+  })
+  .then(() => console.log('cjs build success.'))
+
+esbuild
+  .build({
+    ...common,
+    outfile: 'lib/nostr.bundle.js',
+    format: 'iife',
+    globalName: 'plebai',
+    define: {
+      window: 'self',
+      global: 'self',
+      process: '{"env": {}}',
+    },
+  })
+  .then(() => console.log('standalone build success.'))
