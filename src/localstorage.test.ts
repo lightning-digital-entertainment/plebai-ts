42fc24bf611fc8ef6e615d57abad136fb9046a79
Egge <egge24@protonmail.com>
2023-11-12 07:17:59 +0100

    added localstorage tests

---
+++ src/localstorage.test.ts
@@ -0,0 +1,77 @@
+import { describe, test, expect, afterEach } from '@jest/globals';
+import { signFromLocalstorage } from './messages';
+import { EventTemplate, generatePrivateKey } from 'nostr-tools';
+import { getPublicKeyFromLocalstorage } from './localstorage';
+
+class LocalStorageMock {
+  store: {
+    [key: string]: string;
+  };
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+//@ts-ignore
+global.localStorage = new LocalStorageMock();
+
+describe('signing a message from localstorage', () => {
+  afterEach(() => {
+    localStorage.removeItem('pleb_sk');
+  });
+  test('assume private key is found', () => {
+    localStorage.setItem('pleb_sk', generatePrivateKey());
+    const unsignedEvent: EventTemplate = {
+      created_at: Math.floor(Date.now() / 1000),
+      content: 'Event content',
+      kind: 1,
+      tags: [],
+    };
+    const signedEvent = signFromLocalstorage(unsignedEvent);
+    expect(signedEvent.sig).toBeTruthy();
+  });
+
+  test('assume no private key is found', () => {
+    const unsignedEvent: EventTemplate = {
+      created_at: Math.floor(Date.now() / 1000),
+      content: 'Event content',
+      kind: 1,
+      tags: [],
+    };
+    expect(() => {
+      const signedEvent = signFromLocalstorage(unsignedEvent);
+    }).toThrow();
+  });
+});
+
+describe('getting public key from local storage', () => {
+  afterEach(() => {
+    localStorage.removeItem('pleb_sk');
+  });
+  test('assume private key is found', () => {
+    localStorage.setItem('pleb_sk', generatePrivateKey());
+    const publicKey = getPublicKeyFromLocalstorage();
+    expect(publicKey).toBeTruthy();
+  });
+  test('assume no private key is found', () => {
+    expect(() => {
+      const publicKey = getPublicKeyFromLocalstorage();
+    }).toThrow();
+  });
+});
